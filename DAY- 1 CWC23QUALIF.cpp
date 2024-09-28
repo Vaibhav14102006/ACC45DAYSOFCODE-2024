0a8e6e96327a2d543216c57972fe7cc7a6d42efd
Vaibhav14102006 <vaibhavsinghrajawat483@gmail.com>
2024-09-28 14:18:10 +0530

    Add files via upload

---
+++ DAY- 1 CWC23QUALIF.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int score;
+	cin>>score;;
+	if(score>=12){
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+
+}
