5f06d4591f226471d9543ae49c3d0065b7662f3f
Vaibhav14102006 <vaibhavsinghrajawat483@gmail.com>
2024-09-29 14:04:49 +0530

    Add files via upload

---
+++ DAY 2 SUBSCRIBE_.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, a, b;
+    cin >> t;
+    while (t-- > 0) {
+        cin >> a >> b;
+        if (a % 6 == 0) {
+            cout << (a / 6) * b << endl;
+        } else {
+            cout << ((a / 6) + 1) * b << endl;
+        }
+    }
+    return 0;
+}
