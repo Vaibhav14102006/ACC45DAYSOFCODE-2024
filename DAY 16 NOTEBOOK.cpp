dd94a6f46389b86fd9dfaf860a91307a3aeda75a
Vaibhav14102006 <vaibhavsinghrajawat483@gmail.com>
2024-10-13 11:34:51 +0530

    Add files via upload

---
+++ DAY 16 NOTEBOOK.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;  // Read the number of test cases
+    while (T--) {
+        int N;
+        cin >> N;  // Read the weight of pulp for this test case
+        cout << 10 * N << endl;  // Output the number of notebooks
+    }
+    return 0;
+}
